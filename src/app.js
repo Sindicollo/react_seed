7c299c004528b618da66c35fe3734945798e4958
Alexey Krylov2 <sindicollo@gmail.com>
2017-01-26 15:05:27 +0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+var App = React.createClass ({
+    render: () => (
+        <div>
+            <h1>This is React App Seed</h1>
+            <p>React, webpack and magic!</p>
+        </div>
+    ) 
+});
+
+module.exports = App;
+
